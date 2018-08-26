9e44f864cc2784849448911841c4fa949a6f9f5c
Jared Keown <jkeownwebdev@gmail.com>
2018-08-26 14:16:51 -0700

    use concise declarative functions

---
+++ write-concise-declarative-functions-with-es6.js
@@ -0,0 +1,14 @@
+// CHALLENGE:
+// Refactor the function setGear inside the object bicycle to use the shorthand syntax
+
+// change code below this line
+const bicycle = {
+  gear: 2,
+  setGear(newGear) {
+    "use strict";
+    this.gear = newGear;
+  }
+};
+// change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
