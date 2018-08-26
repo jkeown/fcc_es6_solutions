b884c4b2b9dcc94464b7219b1c1aed91ac843ef3
Jared Keown <jkeownwebdev@gmail.com>
2018-08-26 14:30:18 -0700

    es6 class syntax for constructors

---
+++ use-class-syntax-to-define-a-constructor-function.js
@@ -0,0 +1,17 @@
+// CHALLENGE:
+//Use class keyword and write a proper constructor to create the Vegetable class
+
+function makeClass() {
+  "use strict";
+  /* Alter code below this line */
+class Vegetable {
+  constructor(veggie){
+    this.name = veggie;
+  }
+}
+  /* Alter code above this line */
+  return Vegetable;
+}
+const Vegetable = makeClass();
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
