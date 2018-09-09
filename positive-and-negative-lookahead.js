f6b535b991dcee25ca53430d3a7dff775626c920
Jared Keown <jkeownwebdev@gmail.com>
2018-09-09 10:09:10 -0700

    pos and neg lookahead regex

---
+++ positive-and-negative-lookahead.js
@@ -0,0 +1,7 @@
+// CHALLENGE
+
+// Use lookaheads in the pwRegex to match passwords that are greater than 5 characters long and have two consecutive digits.
+
+let sampleWord = "astronaut";
+let pwRegex = /(?=\w{5,})(?=\D*\d)/; // Change this line
+let result = pwRegex.test(sampleWord);
