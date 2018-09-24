8ed9455f92df2fe9e6789cda61844700b2f96be8
Jared Keown <jkeownwebdev@gmail.com>
2018-09-24 10:45:52 -0700

    Functional Programming: combine array into a string

---
+++ combine-array-into-string.js
@@ -0,0 +1,10 @@
+// Use the join method (among others) inside the sentensify function to make a sentence from the words in the string str. The function should return a string.  For this challenge, do not use the replace method.
+
+function sentensify(str) {
+  // Add your code below this line
+  
+  return str.split(/\W/g).join(' ');
+  
+  // Add your code above this line
+}
+sentensify("May-the-force-be-with-you");
