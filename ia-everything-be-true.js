66ee8a919287a8249fe6975cf84a384f1f56df55
Jared Keown <jkeownwebdev@gmail.com>
2018-10-15 12:52:47 -0700

    Intermediate Algorithms: check truthy or falsy

--- ia-everything-be-true.js
+++ ia-everything-be-true.js
@@ -7,4 +7,3 @@ function truthCheck(collection, pre) {
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
 
-// i like my code for this one
